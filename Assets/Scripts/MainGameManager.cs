b53db2c78fa0e08c158ec28e3f139f37d62a6075
Jakub Duchniewicz <j.duchniewicz@gmail.com>
2022-11-20 01:14:54 +0100

    Integrate into main scene.

--- Assets/Scripts/MainGameManager.cs
+++ Assets/Scripts/MainGameManager.cs
@@ -91,6 +91,7 @@ public class MainGameManager : MightyGameManager
 
     public void TriggerLosingBattle()
     {
+        Debug.Log("You Lose!");
 
     }
 }
--- Assets/Scripts/PotopManager.cs
+++ Assets/Scripts/PotopManager.cs
@@ -6,6 +6,7 @@ public class PotopManager : MonoBehaviour
 {
     // Start is called before the first frame update
     public GameObject[] waves;
+    public GameObject[] dottedWaves;
     public int currentWave = 0;
     public int maxWaves = 5;
 
@@ -13,10 +14,11 @@ public class PotopManager : MonoBehaviour
 
     void Start()
     {
-        mainGameManager = GameObject.Find("MainGameManager").GetComponent<MainGameManager>();
+        mainGameManager = GameObject.Find("GameManager").GetComponent<MainGameManager>();
         for (int i = 1; i < maxWaves; ++i)
         {
             waves[i].SetActive(false);
+            dottedWaves[i].SetActive(false);
         }
         //waves[currentWave].GetComponent<SpriteRenderer>().
     }
@@ -30,6 +32,7 @@ public class PotopManager : MonoBehaviour
     public void AdvanceWave()
     {
         waves[currentWave].SetActive(false);
+        dottedWaves[currentWave].SetActive(false);
         ++currentWave;
         if (currentWave > maxWaves)
         {
@@ -38,6 +41,7 @@ public class PotopManager : MonoBehaviour
         }
         // animations etc for transitions
         waves[currentWave].SetActive(true); // TODO: fade in
+        dottedWaves[currentWave].SetActive(true); // TODO: fade in
     }
 
     public void Restart()
